cd125e94d4a70d97a3f4c4a9591705b05f2e8e3d
Roger Goma <roger.goma@mango.com>
2021-03-03 01:44:47 +0100

    Add heroku needed config

--- Utility/googleAuth.js
+++ Utility/googleAuth.js
@@ -32,7 +32,7 @@ function initAuthorize(callback) {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
   // Check if we have previously stored a token.
